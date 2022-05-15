99127252e0595191997d845cb38dc296e427aa57
Dmitriy <nevermind1979@mail.ru>
2022-05-15 20:53:42 +0300

    add exception

---
+++ src/main/java/com/myCodeReview/dmitriy/task2/exception/ChatException.java
@@ -0,0 +1,7 @@
+package com.myCodeReview.dmitriy.task2.exception;
+
+public class ChatException extends RuntimeException {
+    public ChatException(String message) {
+        super(message);
+    }
+}
